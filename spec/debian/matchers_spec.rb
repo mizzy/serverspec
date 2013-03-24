97713c1dbb153e01a271dec7e8b1053348e349eb
Tatsuya Takamura <takamura@tonchidot.com>
2013-03-24 21:07:23 +0900

    Move duplicated specs to shared_examples

--- spec/debian/matchers_spec.rb
+++ spec/debian/matchers_spec.rb
@@ -3,63 +3,10 @@ require 'spec_helper'
 describe 'Serverspec matchers of Debian family', :serverspec => :debian do
   let(:test_server_host) { 'serverspec-debian-host' }
 
-  describe 'be_enabled' do
-    describe 'rc.local' do
-      it { should be_enabled }
-    end
-
-    describe 'this-is-dummy-service' do
-      it { should_not be_enabled }
-    end
-  end
-
-  describe 'be_installed' do
-    describe 'openssh-server' do
-      it { should be_installed }
-    end
-
-    describe 'this-is-dummy-package' do
-      it { should_not be_installed }
-    end
-  end
-
-  describe 'be_running' do
-    describe 'ssh' do
-      it { should be_running }
-    end
-
-    describe 'this-is-dummy-daemon' do
-      it { should_not be_running }
-    end
-  end
-
-  describe 'be_listening' do
-    describe 'port 22' do
-      it { should be_listening }
-    end
-
-    describe 'port 9999' do
-      it { should_not be_listening }
-    end
-  end
-
-  describe 'be_file' do
-    describe '/etc/ssh/sshd_config' do
-      it { should be_file }
-    end
-
-    describe '/etc/thid_is_dummy_file' do
-      it { should_not be_file }
-    end
-  end
-
-  describe 'contain' do
-    describe '/etc/ssh/sshd_config' do
-      it { should contain 'See the sshd_config(5) manpage for details' }
-    end
-
-    describe '/etc/ssh/sshd_config' do
-      it { should_not contain 'This is duymmy text!!' }
-    end
-  end
+  it_behaves_like 'support be_enabled matcher', 'rc.local'
+  it_behaves_like 'support be_installed matcher', 'openssh-server'
+  it_behaves_like 'support be_running matcher', 'ssh'
+  it_behaves_like 'support be_listening matcher', 22
+  it_behaves_like 'support be_file matcher', '/etc/ssh/sshd_config'
+  it_behaves_like 'support contain matcher', '/etc/ssh/sshd_config', 'See the sshd_config(5) manpage'
 end
--- spec/redhat/matchers_spec.rb
+++ spec/redhat/matchers_spec.rb
@@ -3,63 +3,10 @@ require 'spec_helper'
 describe 'Serverspec matchers of Red Hat family', :serverspec => :redhat do
   let(:test_server_host) { 'serverspec-redhat-host' }
 
-  describe 'be_enabled' do
-    describe 'sshd' do
-      it { should be_enabled }
-    end
-
-    describe 'this-is-dummy-service' do
-      it { should_not be_enabled }
-    end
-  end
-
-  describe 'be_installed' do
-    describe 'openssh' do
-      it { should be_installed }
-    end
-
-    describe 'this-is-dummy-package' do
-      it { should_not be_installed }
-    end
-  end
-
-  describe 'be_running' do
-    describe 'sshd' do
-      it { should be_running }
-    end
-
-    describe 'this-is-dummy-daemon' do
-      it { should_not be_running }
-    end
-  end
-
-  describe 'be_listening' do
-    describe 'port 22' do
-      it { should be_listening }
-    end
-
-    describe 'port 9999' do
-      it { should_not be_listening }
-    end
-  end
-
-  describe 'be_file' do
-    describe '/etc/ssh/sshd_config' do
-      it { should be_file }
-    end
-
-    describe '/etc/thid_is_dummy_file' do
-      it { should_not be_file }
-    end
-  end
-
-  describe 'contain' do
-    describe '/etc/ssh/sshd_config' do
-      it { should contain 'This is the sshd server system-wide configuration file' }
-    end
-
-    describe '/etc/ssh/sshd_config' do
-      it { should_not contain 'This is duymmy text!!' }
-    end
-  end
+  it_behaves_like 'support be_enabled matcher', 'sshd'
+  it_behaves_like 'support be_installed matcher', 'openssh'
+  it_behaves_like 'support be_running matcher', 'sshd'
+  it_behaves_like 'support be_listening matcher', 22
+  it_behaves_like 'support be_file matcher', '/etc/ssh/sshd_config'
+  it_behaves_like 'support contain matcher', '/etc/ssh/sshd_config', 'This is the sshd server system-wide configuration file'
 end
