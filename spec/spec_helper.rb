02a8890c9dcda3886f220aae737ad59143a23c3d
Gosuke Miyashita <gosukenator@gmail.com>
2013-03-30 00:56:47 +0900

    Rspec.configure is not needed currently

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,8 +5,3 @@ PROJECT_ROOT = (Pathname.new(File.dirname(__FILE__)) + '..').expand_path
 
 Dir[PROJECT_ROOT.join("spec/support/**/*.rb")].each { |file| require(file) }
 
-RSpec.configure do |c|
-  c.before do
-    c.host = test_server_host
-  end
-end
