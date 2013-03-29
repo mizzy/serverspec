5e87d696b75fb148f1d0fb1311e5b56ce52b2ee9
Gosuke Miyashita <gosukenator@gmail.com>
2013-03-29 09:51:12 +0900

    Some fixes for reuse SSH version

--- lib/serverspec/matchers/be_linked_to.rb
+++ lib/serverspec/matchers/be_linked_to.rb
@@ -1,6 +1,6 @@
 RSpec::Matchers.define :be_linked_to do |target|
   match do |link|
-    ret = ssh_exec(RSpec.configuration.host, commands.check_link(link, target))
+    ret = ssh_exec(commands.check_link(link, target))
     ret[:exit_code] == 0
   end
 end
--- lib/serverspec/matchers/have_cron_entry.rb
+++ lib/serverspec/matchers/have_cron_entry.rb
@@ -1,7 +1,7 @@
 RSpec::Matchers.define :have_cron_entry do |entry|
   match do |actual|
     @user ||= 'root'
-    ret = ssh_exec(RSpec.configuration.host, commands.check_cron_entry(@user, entry))
+    ret = ssh_exec(commands.check_cron_entry(@user, entry))
     ret[:exit_code] == 0
   end
   chain :with_user do |user|
