13e7032323ee5733de16f7ef66b21fc2b15f7f44
Gosuke Miyashita <gosukenator@gmail.com>
2013-03-30 16:06:38 +0900

    Change setup method

--- lib/serverspec/setup.rb
+++ lib/serverspec/setup.rb
@@ -3,42 +3,64 @@ require 'fileutils'
 module Serverspec
   class Setup
     def self.run
-      %w( spec spec/www.example.jp ).each { |dir| safe_mkdir(dir) }
+      print "Input target host name: "
+      @hostname = gets.chomp
+
+      prompt = <<-EOF
+
+Select OS Type of target host:
+
+  1) Red Hat
+  2) Debian
+  3) Gentoo
+  4) Solaris
+  5) None
+
+Select container number: 
+EOF
+
+      print prompt.chop
+      num = gets.to_i - 1
+      puts
+
+      @os_type = [ 'RedHat', 'Debian', 'Gentoo', 'Solaris', nil ][num]
+      [ 'spec', "spec/#{@hostname}" ].each { |dir| safe_mkdir(dir) }
       safe_create_spec
       safe_create_spec_helper
       safe_create_rakefile
     end
 
     def self.safe_create_spec
-            content = <<-EOF
+
+      content = <<-EOF
 require 'spec_helper'
 
-describe 'httpd', :os => :redhat do
+describe 'httpd' do
   it { should be_installed }
   it { should be_enabled   }
   it { should be_running   }
 end
 
-describe 'port 80', :os => :redhat do
+describe 'port 80' do
   it { should be_listening }
 end
 
-describe '/etc/httpd/conf/httpd.conf', :os => :redhat do
+describe '/etc/httpd/conf/httpd.conf' do
   it { should be_file }
-  it { should contain "ServerName www.example.jp" }
+  it { should contain "ServerName #{@hostname}" }
 end
 EOF
 
-      if File.exists? 'spec/www.example.jp/httpd_spec.rb'
-        old_content = File.read('spec/www.example.jp/httpd_spec.rb')
+      if File.exists? "spec/#{@hostname}/httpd_spec.rb"
+        old_content = File.read("spec/#{@hostname}/httpd_spec.rb")
         if old_content != content
-          $stderr.puts "!! spec/www.example.jp/httpd_spec.rb already exists and differs from template"
+          $stderr.puts "!! spec/#{@hostname}/httpd_spec.rb already exists and differs from template"
         end
       else
-        File.open('spec/www.example.jp/httpd_spec.rb', 'w') do |f|
+        File.open("spec/#{@hostname}/httpd_spec.rb", 'w') do |f|
           f.puts content
         end
-        puts ' + spec/www.example.jp/httpd_spec.rb'
+        puts " + spec/#{@hostname}/httpd_spec.rb"
       end
     end
 
@@ -60,6 +82,7 @@ require 'pathname'
 require 'net/ssh'
 
 RSpec.configure do |c|
+  ### include os helper ###
   c.before do
     host  = File.basename(Pathname.new(example.metadata[:location]).dirname)
     if c.host != host
@@ -72,6 +95,11 @@ RSpec.configure do |c|
   end
 end
 EOF
+
+        if not @os_type.nil?
+          content.gsub!(/### include os helper ###/, "c.include(Serverspec::#{@os_type}Helper)")
+        end
+
       if File.exists? 'spec/spec_helper.rb'
         old_content = File.read('spec/spec_helper.rb')
         if old_content != content
