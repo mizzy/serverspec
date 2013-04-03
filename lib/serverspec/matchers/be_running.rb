b8844586e7c0fff7f9464e49752c31f82624adea
Gosuke Miyashita <gosukenator@gmail.com>
2013-04-03 18:20:56 +0900

    Rename actual to process for readability

--- lib/serverspec/matchers/be_running.rb
+++ lib/serverspec/matchers/be_running.rb
@@ -1,8 +1,8 @@
 RSpec::Matchers.define :be_running do
-  match do |actual|
-    ret = ssh_exec(commands.check_running(actual))
+  match do |process|
+    ret = ssh_exec(commands.check_running(process))
     if ret[:exit_code] == 1 || ret[:stdout] =~ /stopped/
-      ret = ssh_exec(commands.check_process(actual))
+      ret = ssh_exec(commands.check_process(process))
     end
     ret[:exit_code] == 0
   end
