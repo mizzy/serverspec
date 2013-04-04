0f5ed166f1b55cbee23f479bd6c5f3c1e85e05dc
Raphaël Pinson <raphael.pinson@camptocamp.com>
2013-04-04 23:57:59 +0200

    Refactor to support both Exec backend in addition to Ssh

--- lib/serverspec/helper.rb
+++ lib/serverspec/helper.rb
@@ -41,7 +41,16 @@ module Serverspec
       ssh.loop
       { :stdout => stdout_data, :stderr => stderr_data, :exit_code => exit_code, :exit_signal => exit_signal }
     end
+  end
 
+  module ExecHelper
+    def ssh_exec(cmd, opts={})
+      stdout = `#{cmd} 2>&1`
+      # In ruby 1.9, it is possible to use Open3.capture3, but not in 1.8
+      #stdout, stderr, status = Open3.capture3(cmd)
+      { :stdout => stdout, :stderr => nil,
+        :exit_code => $?, :exit_signal => nil }
+    end
   end
 
   module RedHatHelper
