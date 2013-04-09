146759e5a25f58a26c23c2528b35a55aa24f929b
Gosuke Miyashita <gosukenator@gmail.com>
2013-04-09 15:16:59 +0900

    The style of be_running implemented by kentaro is cool. So I implemented be_isntalled like that.

--- lib/serverspec/matchers/be_installed.rb
+++ lib/serverspec/matchers/be_installed.rb
@@ -2,14 +2,23 @@ RSpec::Matchers.define :be_installed do
   match do |name|
     if @provider.nil?
       backend.check_installed(name)
-    elsif @provider == 'gem'
-      backend.check_installed_by_gem(name, @version)
+    else
+      check_method = "check_installed_by_#{@provider}".to_sym
+
+      unless backend.respond_to?(check_method)
+        raise ArgumentError.new("`be_installed` matcher doesn't support #{@under}")
+      end
+
+      backend.send(check_method, name, @version)
     end
   end
+
   chain :by do |provider|
     @provider = provider
   end
+
   chain :with_version do |version|
     @version = version
   end
+
 end
