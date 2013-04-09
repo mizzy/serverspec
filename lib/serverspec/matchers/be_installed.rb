fbc0c18d5f32e15515ff3617e39b2e8893b184a7
Gosuke Miyashita <gosukenator@gmail.com>
2013-04-09 14:52:06 +0900

    Support "by" chain with be_installed matcher

--- lib/serverspec/matchers/be_installed.rb
+++ lib/serverspec/matchers/be_installed.rb
@@ -1,5 +1,15 @@
 RSpec::Matchers.define :be_installed do
-  match do |actual|
-    backend.check_installed(actual)
+  match do |name|
+    if @provider.nil?
+      backend.check_installed(name)
+    elsif @provider == 'gem'
+      backend.check_installed_by_gem(name, @version)
+    end
+  end
+  chain :by do |provider|
+    @provider = provider
+  end
+  chain :with_version do |version|
+    @version = version
   end
 end
