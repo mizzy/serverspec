db7f2cfae5eaeac7f8ec3dcf0bd98dafd9d8f500
Gosuke Miyashita <gosukenator@gmail.com>
2013-04-12 00:13:15 +0900

    Add Serverspec::Helper::OSDetect

--- lib/serverspec/helper.rb
+++ lib/serverspec/helper.rb
@@ -10,6 +10,7 @@ require 'serverspec/helper/redhat'
 require 'serverspec/helper/debian'
 require 'serverspec/helper/gentoo'
 require 'serverspec/helper/solaris'
+require 'serverspec/helper/os_detect'
 
 # Obsoleted helpers
 require 'serverspec/helper/obsoleted'
