b6b185b130b4b90536b5a760683737225e2948d0
Gosuke Miyashita <gosukenator@gmail.com>
2013-04-23 18:50:36 +0900

    Show deprecated message when using get_stdout matcher

--- lib/serverspec/matchers/get_stdout.rb
+++ lib/serverspec/matchers/get_stdout.rb
@@ -1,5 +1,13 @@
 RSpec::Matchers.define :get_stdout do |expected|
   match do |command|
+      puts <<'EOF'
+
+************************************
+"get_stdout" matcher is deprecated.
+Use "return_stdout" matcher instead.
+************************************
+
+EOF
     ret = backend.do_check(command)
     ret[:stdout] =~ /#{expected}/
   end
