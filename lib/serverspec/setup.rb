0c3b1545b9746c21d99e760bec26219b0c65e863
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2013-04-23 15:14:13 +0900

    ask sudo password

--- lib/serverspec/setup.rb
+++ lib/serverspec/setup.rb
@@ -101,7 +101,12 @@ require 'pathname'
 ### include requirements ###
 
 RSpec.configure do |c|
-  c.sudo_password = ENV['SUDO_PASSWORD']
+  if ENV['ASK_SUDO_PASSWORD']
+    require 'highline/import'
+    c.sudo_password = ask("Enter sudo password: ") { |q| q.echo = false }
+  else
+    c.sudo_password = ENV['SUDO_PASSWORD']
+  end
   ### include backend helper ###
   ### include os helper ###
   ### include backend conf ###
