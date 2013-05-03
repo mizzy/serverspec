9e42884bf31fe68108f054b1ef1237f943ff0e11
Gosuke Miyashita <gosukenator@gmail.com>
2013-05-04 02:11:14 +0900

    Add with and only_with chain to be_mounted matcher

--- lib/serverspec/matchers/be_mounted.rb
+++ lib/serverspec/matchers/be_mounted.rb
@@ -1,5 +1,13 @@
 RSpec::Matchers.define :be_mounted do
   match do |path|
-    backend.check_mounted(example, path)
+    backend.check_mounted(example, path, @attr, @only_with)
+  end
+  chain :with do |attr|
+    @attr      = attr
+    @only_with = false
+  end
+  chain :only_with do |attr|
+    @attr      = attr
+    @only_with = true
   end
 end
--- spec/gentoo/matchers_spec.rb
+++ spec/gentoo/matchers_spec.rb
@@ -7,7 +7,11 @@ describe 'Serverspec matchers of Gentoo family', :os => :gentoo do
   it_behaves_like 'support be_running.under("supervisor") matcher', 'growthforecast'
   it_behaves_like 'support be_listening matcher', 22
   it_behaves_like 'support be_file matcher', '/etc/ssh/sshd_config'
+
   it_behaves_like 'support be_mounted matcher', '/'
+  it_behaves_like 'support be_mounted.with matcher', '/'
+  it_behaves_like 'support be_mounted.only_with matcher', '/'
+
   it_behaves_like 'support be_resolvable matcher', 'localhost'
   it_behaves_like 'support be_resolvable.by matcher', 'localhost', 'hosts'
   it_behaves_like 'support be_resolvable.by matcher', 'localhost', 'dns'
