6a225da7c8043c2700ac7ecd61e8cd82aab365c1
Gosuke Miyashita <gosukenator@gmail.com>
2013-05-06 01:38:18 +0900

    Revert "Hack for getting stdout, stderr, exit status in 1.8"
    SSH with pty merge stderr to stdout, so do same thing with local exec.
    
    This reverts commit 043e769aa6e656f8034e7cac839b5e8e549b5a1a.
    
    Conflicts:
    
    	lib/serverspec/backend/exec.rb

--- lib/serverspec/backend/exec.rb
+++ lib/serverspec/backend/exec.rb
@@ -1,5 +1,3 @@
-require 'open3'
-
 module Serverspec
   module Backend
     class Exec
@@ -12,17 +10,11 @@ module Serverspec
       end
 
       def run_command(cmd, opts={})
+        stdout = `#{cmd} 2>&1`
         # In ruby 1.9, it is possible to use Open3.capture3, but not in 1.8
         #stdout, stderr, status = Open3.capture3(cmd)
-        # So get exit status with `command`
-        `#{cmd} 2>&1`
-        ret = { :exit_status => $?, :exit_signal => nil }
-
-        # Get stdout and stderr
-        stdin, stdout, stderr = Open3.popen3(cmd)
-        ret[:stdout] = stdout.read
-        ret[:stderr] = stderr.read
-        ret
+        { :stdout => stdout, :stderr => nil,
+          :exit_status => $?, :exit_signal => nil }
       end
 
       def check_zero(cmd, *args)
