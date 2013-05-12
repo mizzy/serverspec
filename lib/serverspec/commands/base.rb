25c5596275f2dd73e3f8104b51306682247ad8c2
Francisco A. Lozano <francisco.lozano@kii.com>
2013-05-13 00:16:12 +0200

    spec tests passing

--- lib/serverspec/commands/base.rb
+++ lib/serverspec/commands/base.rb
@@ -173,7 +173,8 @@ module Serverspec
       end
 
       def check_access_by_user user, access, file
-        raise NotImplementedError.new
+        # Tested on MacOS X. Specific linux-only impl also available
+        "sudo -u #{user} -s /bin/test -#{access} #{file}"
       end
     end
   end
--- lib/serverspec/commands/linux.rb
+++ lib/serverspec/commands/linux.rb
@@ -6,7 +6,10 @@ module Serverspec
       class NotImplementedError < Exception; end
 
       def check_access_by_user user, access, file
-        "su -s /bin/sh -c \"test -#{access} #{file}\" #{user}"
+        # - Maybe it could also use the darwin one...
+        # but using runuser bcs in linux it's common to change the default sudo configuration.
+        # - Using specific shell to avoid system users not logging in
+        "runuser -s /bin/sh -c \"test -#{access} #{file}\" #{user}"
       end
 
       def check_iptables_rule rule, table=nil, chain=nil
