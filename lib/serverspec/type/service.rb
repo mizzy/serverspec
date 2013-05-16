bd35380e982e2ec0ce6f5d7dfaddc22b64af1532
Gosuke Miyashita <gosukenator@gmail.com>
2013-05-17 08:58:51 +0900

    Support fluent interface with chain for type explicit subject

--- lib/serverspec/type/service.rb
+++ lib/serverspec/type/service.rb
@@ -4,9 +4,25 @@ module Serverspec
       def initialize name
         @name = name
       end
+
       def enabled?
         backend.check_enabled(nil, @name)
       end
+
+      def running? under
+        if under
+          check_method = "check_running_under_#{under}".to_sym
+
+          unless backend.respond_to?(check_method)
+            raise ArgumentError.new("`be_running` matcher doesn't support #{@under}")
+          end
+
+          backend.send(check_method, nil, @name)
+        else
+          backend.check_running(nil, @name)
+        end
+      end
+
     end
   end
 end
