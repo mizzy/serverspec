7999ad88db110183d0e26c50f9ec4410260ff7a2
Gosuke Miyashita <gosukenator@gmail.com>
2013-05-17 20:18:06 +0900

    Add contain matcher for file type

--- lib/serverspec/type/file.rb
+++ lib/serverspec/type/file.rb
@@ -8,6 +8,14 @@ module Serverspec
       def directory?
         backend.check_directory(nil, @name)
       end
+
+      def contain(pattern, from, to)
+        if (@from || @to).nil?
+          cmd = backend.check_file_contain(nil, @name, pattern)
+        else
+          cmd = backend.check_file_contain_within(nil, @name, pattern, from, to)
+        end
+      end
     end
   end
 end
