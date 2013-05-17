dd7b22ceb35e4e1611c597c11fba399ca701163d
Gosuke Miyashita <gosukenator@gmail.com>
2013-05-17 17:28:57 +0900

    Add specs for package type

---
+++ spec/support/shared_package_examples.rb
@@ -0,0 +1,41 @@
+shared_examples_for 'support package installed matcher' do |name|
+  describe 'be_installed' do
+    describe package(name) do
+      it { should be_installed }
+    end
+
+    describe 'invalid-package' do
+      it { should_not be_installed }
+    end
+  end
+end
+
+shared_examples_for 'support package installed by gem matcher' do |name|
+  describe 'installed by gem' do
+    describe package(name) do
+      it { should be_installed.by('gem') }
+    end
+
+    describe 'invalid-gem' do
+      it { should_not be_installed.by('gem') }
+    end
+  end
+end
+
+shared_examples_for 'support package installed by gem with version matcher' do |name, version|
+  describe 'installed by gem with version' do
+    before :all do
+      RSpec.configure do |c|
+        c.stdout = "#{name} (#{version})"
+      end
+    end
+
+    describe package(name) do
+      it { should be_installed.by('gem').with_version(version) }
+    end
+
+    describe package(name) do
+      it { should_not be_installed.by('gem').with_version('invalid-version') }
+    end
+  end
+end
