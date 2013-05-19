35e99575466bda0b2608937e69b7106eaaf6e96d
Gosuke Miyashita <gosukenator@gmail.com>
2013-05-19 18:07:54 +0900

    Explcit ipnat subject type

--- lib/serverspec/helper/type.rb
+++ lib/serverspec/helper/type.rb
@@ -3,7 +3,7 @@ module Serverspec
     module Type
       types = %w(
         base service package port file cron command linux_kernel_parameter iptables host
-        routing_table default_gateway selinux user group zfs
+        routing_table default_gateway selinux user group zfs ipnat
       )
 
       types.each {|type| require "serverspec/type/#{type}" }
