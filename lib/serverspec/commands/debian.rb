e225b13ec8c92d84b3fa7d82dd0a745cee184881
travis <travis@cloudscaling.com>
2013-05-26 12:58:34 -0400

    Fix Debian service detection
    * check_enabled now understands upstart (enough to
      check whether services start on boot)
    
    * check_running now properly uses /usr/sbin/service

--- lib/serverspec/commands/debian.rb
+++ lib/serverspec/commands/debian.rb
@@ -2,11 +2,17 @@ module Serverspec
   module Commands
     class Debian < Linux
       def check_enabled service
-        "ls /etc/rc3.d/ | grep -- #{escape(service)}"
+        # Until everything uses Upstart, this needs an OR.
+        "ls /etc/rc3.d/ | grep -- #{escape(service)} || grep 'start on' /etc/init/#{escape(service)}.conf"      
       end
 
       def check_installed package
         "dpkg -s #{escape(package)}"
+      end
+
+      def check_running service
+        # This is compatible with Debian >Jaunty and Ubuntu derivatives
+        "/usr/sbin/service #{escape(service)} status | grep 'running'"
       end
     end
   end
