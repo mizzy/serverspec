1f574c92bc11a73421e2d7f250952af11d8e71ad
youyo <contact@youyo.info>
2013-06-07 10:35:17 +0900

    Fixed SELinux check.

--- lib/serverspec/commands/linux.rb
+++ lib/serverspec/commands/linux.rb
@@ -19,7 +19,7 @@ module Serverspec
       end
 
       def check_selinux mode
-        "getenforce | grep -i -- #{escape(mode)} && grep -i -- SELINUX=#{escape(mode)} /etc/selinux/config"
+        "getenforce | grep -i -- #{escape(mode)} && grep -i -- ^SELINUX=#{escape(mode)} /etc/selinux/config"
       end
     end
   end
