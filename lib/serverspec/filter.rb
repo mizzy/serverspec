617e3f0182363c38147e77423c360b64020a5b50
Gosuke Miyashita <gosukenator@gmail.com>
2013-06-10 11:10:07 +0900

    Use Comands::Base automatically before OS detection

--- lib/serverspec/filter.rb
+++ lib/serverspec/filter.rb
@@ -6,7 +6,7 @@ module Serverspec
       # Linux kernel parameters
       %w( abi crypto debug dev fs kernel net sunrpc vm ).each do |param|
         if description_args.match(/^#{param}\./)
-          ret = backend(Serverspec::Commands::Base).run_command("/sbin/sysctl -q -n #{description_args}")
+          ret = backend.run_command("/sbin/sysctl -q -n #{description_args}")
           val = ret[:stdout].strip
           val = val.to_i if val.match(/^\d+$/)
           subject = Serverspec::Subject.new
--- lib/serverspec/helper/detect_os.rb
+++ lib/serverspec/helper/detect_os.rb
@@ -2,7 +2,8 @@ module Serverspec
   module Helper
     module DetectOS
       def commands
-        self.class.const_get('Serverspec').const_get('Commands').const_get(RSpec.configuration.os).new
+        os = RSpec.configuration.os || 'Base'
+        self.class.const_get('Serverspec').const_get('Commands').const_get(os).new
       end
     end
   end
