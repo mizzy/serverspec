f90202c24c4ec50f3fc2c8e21d9f5008b55f1f8d
Gosuke Miyashita <gosukenator@gmail.com>
2013-06-12 00:04:13 +0900

    Remove be_mode and be_owned_by

--- lib/serverspec/matchers/be_owned_by.rb
+++
@@ -1,9 +0,0 @@
-RSpec::Matchers.define :be_owned_by do |owner|
-  match do |file|
-    if file.respond_to?(:owned_by?)
-      file.owned_by?(owner)
-    else
-      backend.check_owner(file, owner)
-    end
-  end
-end
