5f371948fa6f3b73fea4035267e7008ab0bca7c2
Gosuke Miyashita <gosukenator@gmail.com>
2013-06-11 23:31:32 +0900

    Remove matcher/be_disabled and matcher/be_enabled

--- lib/serverspec/matchers/be_disabled.rb
+++
@@ -1,9 +0,0 @@
-RSpec::Matchers.define :be_disabled do
-  match do |subject|
-    if subject.respond_to?(:disabled?)
-      subject.disabled?
-    else
-      backend.check_selinux('disabled')
-    end
-  end
-end
--- lib/serverspec/matchers/be_enabled.rb
+++
@@ -1,9 +0,0 @@
-RSpec::Matchers.define :be_enabled do
-  match do |name|
-    if name.respond_to?(:enabled?)
-      name.enabled?
-    else
-      backend.check_enabled(name)
-    end
-  end
-end
