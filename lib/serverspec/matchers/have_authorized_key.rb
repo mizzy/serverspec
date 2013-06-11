5575b257a02647d7159bf99efd36ae315987e34f
Gosuke Miyashita <gosukenator@gmail.com>
2013-06-11 23:43:44 +0900

    Remove have_authorized_key, have_home_directory and have_login_shell

--- lib/serverspec/matchers/have_authorized_key.rb
+++
@@ -1,9 +0,0 @@
-RSpec::Matchers.define :have_authorized_key do |key|
-  match do |user|
-    if user.respond_to?(:has_authorized_key?)
-      user.has_authorized_key?(key)
-    else
-      backend.check_authorized_key(user, key)
-    end
-  end
-end
