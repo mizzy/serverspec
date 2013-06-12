667f5bd42e67cfaa075e85e98ade8cc53477cde2
Gosuke Miyashita <gosukenator@gmail.com>
2013-06-13 01:07:06 +0900

    Fix contain

--- lib/serverspec/matchers/contain.rb
+++ lib/serverspec/matchers/contain.rb
@@ -1,16 +1,6 @@
 RSpec::Matchers.define :contain do |pattern|
   match do |file|
-    if file.respond_to?(:contain)
-      file.contain(pattern, @from, @to)
-    else
-      if (@from || @to).nil?
-        cmd = backend.commands.check_file_contain(file, pattern)
-      else
-        cmd = backend.commands.check_file_contain_within(file, pattern, @from, @to)
-      end
-      ret = backend.run_command(cmd)
-      ret[:exit_status] == 0
-    end
+    file.contain(pattern, @from, @to)
   end
   # for contain(pattern).from(/A/).to(/B/)
   chain :from do |from|
