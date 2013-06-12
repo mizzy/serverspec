d9327ff00e5f1752010c4a71d6a77e17892ff76b
Gosuke Miyashita <gosukenator@gmail.com>
2013-06-13 01:05:08 +0900

    Fix return_xxxx

--- lib/serverspec/matchers/return_exit_status.rb
+++ lib/serverspec/matchers/return_exit_status.rb
@@ -1,10 +1,5 @@
 RSpec::Matchers.define :return_exit_status do |status|
   match do |command|
-    if command.respond_to?(:return_exit_status?)
-      command.return_exit_status?(status)
-    else
-      ret = backend.run_command(command)
-      ret[:exit_status].to_i == status
-    end
+    command.return_exit_status?(status)
   end
 end
