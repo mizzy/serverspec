ea14bf4781b4a1e17253bf4f648cbfbd1e94c642
Gosuke Miyashita <gosukenator@gmail.com>
2013-06-13 01:14:52 +0900

    Arrange the order of lines

--- lib/serverspec/matchers.rb
+++ lib/serverspec/matchers.rb
@@ -1,17 +1,30 @@
+# file
 require 'serverspec/matchers/be_mounted'
-require 'serverspec/matchers/be_resolvable'
-require 'serverspec/matchers/be_reachable'
-require 'serverspec/matchers/be_installed'
-require 'serverspec/matchers/be_running'
 require 'serverspec/matchers/contain'
-require 'serverspec/matchers/have_entry'
-require 'serverspec/matchers/belong_to_group'
 require 'serverspec/matchers/be_readable'
 require 'serverspec/matchers/be_writable'
 require 'serverspec/matchers/be_executable'
+require 'serverspec/matchers/match_md5checksum'
+
+# host
+require 'serverspec/matchers/be_resolvable'
+require 'serverspec/matchers/be_reachable'
+
+# package
+require 'serverspec/matchers/be_installed'
+
+# service
+require 'serverspec/matchers/be_running'
+
+# user
+require 'serverspec/matchers/belong_to_group'
+
 require 'serverspec/matchers/return_exit_status'
 require 'serverspec/matchers/return_stdout'
 require 'serverspec/matchers/return_stderr'
-require 'serverspec/matchers/match_md5checksum'
+
+# ipfiter, ipnat, iptables
 require 'serverspec/matchers/have_rule'
 
+# cron, routing_table
+require 'serverspec/matchers/have_entry'
