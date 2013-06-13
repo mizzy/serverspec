cb620ee1a79a06da02fef286d57e0ec3e23f066e
Gosuke Miyashita <gosukenator@gmail.com>
2013-06-13 22:54:10 +0900

    Add OS type caching to Helper::DetectOS

--- lib/serverspec/helper.rb
+++ lib/serverspec/helper.rb
@@ -15,6 +15,7 @@ require 'serverspec/helper/detect_os'
 
 # Attributes helper
 require 'serverspec/helper/attributes'
+include Serverspec::Helper::Attributes
 
 # Subject type helper
 require 'serverspec/helper/type'
