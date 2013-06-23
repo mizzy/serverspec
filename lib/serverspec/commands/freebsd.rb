52a55108d991b61b576a6aa16899b92a2be2f682
Takahiro OKUMURA <hfm.garden@gmail.com>
2013-06-23 17:14:40 +0900

    Remove an unnecessary line because already defined in Base class.

--- lib/serverspec/commands/freebsd.rb
+++ lib/serverspec/commands/freebsd.rb
@@ -3,7 +3,18 @@ require 'shellwords'
 module Serverspec
   module Commands
     class FreeBSD < Base
-      class NotImplementedError < Exception; end
+      def check_enabled service
+        "service -e | grep -- #{escape(service)}"
+      end
+
+      def check_installed package
+        "pkg_version -X -s #{escape(package)}"
+      end
+
+      def check_listening port
+        regexp = ":#{port} "
+        "sockstat -46l -p #{port} | grep -- #{escape(regexp)}"
+      end
     end
   end
 end
