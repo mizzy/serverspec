9efd1af90492d73d6aea885267c1370e5c5954e2
Takahiro OKUMURA <hfm.garden@gmail.com>
2013-06-23 18:10:26 +0900

    Add parentheses according to Ruby Styleguide.

--- lib/serverspec/commands/freebsd.rb
+++ lib/serverspec/commands/freebsd.rb
@@ -3,15 +3,15 @@ require 'shellwords'
 module Serverspec
   module Commands
     class FreeBSD < Base
-      def check_enabled service
+      def check_enabled(service)
         "service -e | grep -- #{escape(service)}"
       end
 
-      def check_installed package
+      def check_installed(package)
         "pkg_version -X -s #{escape(package)}"
       end
 
-      def check_listening port
+      def check_listening(port)
         regexp = ":#{port} "
         "sockstat -46l -p #{port} | grep -- #{escape(regexp)}"
       end
