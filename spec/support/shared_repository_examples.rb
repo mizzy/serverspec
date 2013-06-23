abeb3aa758eda42a8514e09875a86973600c4250
Keisuke KITA <kei.kita2501@gmail.com>
2013-06-23 19:00:38 +0900

    fix typo

--- spec/support/shared_repository_examples.rb
+++ spec/support/shared_repository_examples.rb
@@ -10,7 +10,7 @@ shared_examples_for 'support repository exist matcher' do |valid_repository|
   end
 end
 
-shared_examples_for 'support repository enabled matcher' do |valid_repository|
+shared_examples_for 'support repository be_enabled matcher' do |valid_repository|
   describe 'be_enabled' do
     describe repository(valid_repository) do
       it { should be_enabled }
