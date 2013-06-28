2c189759c878d304f3ce925122680242b7d71133
Gosuke Miyashita <gosukenator@gmail.com>
2013-06-28 12:23:03 +0900

    Use `let` to set stdout for simplifying specs

--- spec/support/shared_interface_examples.rb
+++ spec/support/shared_interface_examples.rb
@@ -1,11 +1,6 @@
 shared_examples_for 'support interface matcher' do |name|
   describe 'interface' do
-    before :all do
-      RSpec.configure do |c|
-        c.stdout = "1000"
-      end
-    end
-
+    let(:stdout) { '1000' }
     describe interface(name) do
       its(:speed) { should eq 1000 }
     end
--- spec/support/shared_service_examples.rb
+++ spec/support/shared_service_examples.rb
@@ -21,11 +21,7 @@ shared_examples_for 'support service running matcher' do |valid_service|
     end
 
     describe service(valid_service) do
-      before :all do
-        RSpec.configure do |c|
-          c.stdout = "#{valid_service} is stopped\r\n"
-        end
-      end
+      let(:stdout) { "#{valid_service} is stopped\r\n" }
       it { should be_running }
     end
   end
@@ -34,22 +30,12 @@ end
 shared_examples_for 'support service running under supervisor matcher' do |valid_service|
   describe 'be_running.under("supervisor")' do
     describe service(valid_service) do
-      before :all do
-        RSpec.configure do |c|
-          c.stdout = "#{valid_service} RUNNING\r\n"
-        end
-      end
-
+      let(:stdout) { "#{valid_service} RUNNING\r\n" }
       it { should be_running.under('supervisor') }
     end
 
     describe service(valid_service) do
-      before :all do
-        RSpec.configure do |c|
-          c.stdout = "#{valid_service} STOPPED\r\n"
-        end
-      end
-
+      let(:stdout) { "#{valid_service} STOPPED\r\n" }
       it { should_not be_running.under('supervisor') }
     end
 
