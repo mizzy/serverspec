d0972e7c8d74711e6980e5387891cdf248bf30dd
sawanoboly <sawanoboriyu@higanworks.com>
2013-06-30 16:07:49 +0900

    pkgin list command

--- lib/serverspec/commands/smartos.rb
+++ lib/serverspec/commands/smartos.rb
@@ -2,7 +2,7 @@ module Serverspec
   module Commands
     class SmartOS < Serverspec::Commands::Solaris
       def check_installed(package, version=nil)
-        cmd = "/opt/local/bin/pkgin 2> /dev/null"
+        cmd = "/opt/local/bin/pkgin list 2> /dev/null"
         if version
           cmd = "#{cmd} | grep -qw -- #{escape(version)}"
         end
