108a74836fb278709184b42d3bf8e5478e0cfab2
Keisuke KITA <kei.kita2501@gmail.com>
2013-07-02 10:51:05 +0900

    Add specs

--- spec/gentoo/service_spec.rb
+++ spec/gentoo/service_spec.rb
@@ -8,4 +8,6 @@ describe 'Serverspec service matchers of Gentoo family' do
   it_behaves_like 'support service running matcher', 'sshd'
   it_behaves_like 'support service running under supervisor matcher', 'sshd'
   it_behaves_like 'support service running under unimplemented matcher', 'sshd'
+  it_behaves_like 'support service monitored by monit matcher', 'unicorn'
+  it_behaves_like 'support service monitored by unimplemented matcher', 'unicorn'
 end
