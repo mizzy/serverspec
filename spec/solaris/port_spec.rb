819e8641033a498d0ec83732063c5ece9fcfbcc4
Keisuke KITA <kei.kita2501@gmail.com>
2013-07-04 20:18:59 +0900

    Add specs

--- spec/solaris/port_spec.rb
+++ spec/solaris/port_spec.rb
@@ -10,3 +10,16 @@ end
 describe port('invalid') do
   it { should_not be_listening }
 end
+
+describe port(80) do
+  it { should be_listening.with(:tcp) }
+  its(:command) { should eq %q!netstat -an 2> /dev/null | egrep 'LISTEN|Idle' | grep -- tcp\\ .\\*.80\\ ! }
+end
+
+describe port(80) do
+  it { 
+    expect {
+      should be_listening.with('not implemented')
+    }.to raise_error(ArgumentError, %r/\A`be_listening` matcher doesn\'t support/)
+  }
+end
