d7a48ca54e44df0396f4259dd61f173e46328992
Yuichiro Saito <koemu@mx.koemu.com>
2013-07-05 16:43:51 +0900

    Updated php_config test codes.

--- spec/solaris/php_config_spec.rb
+++ spec/solaris/php_config_spec.rb
@@ -1,3 +1,10 @@
 require 'spec_helper'
 
 include Serverspec::Helper::Solaris
+
+describe 'Serverspec php_config matchers of Solaris family' do
+  it_behaves_like 'support explicit php_config checking with string', 'default_mimetype', 'text/html'
+  it_behaves_like 'support explicit php_config checking with integer', 'session.cache_expire', 180
+  it_behaves_like 'support explicit php_config checking with regexp',  'mbstring.http_output_conv_mimetypes', /application/
+end
+
