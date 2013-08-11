80c3db1b2f3df02faaac06c8f2b5dffdba35577a
pat <podenski@me.com>
2013-08-11 11:20:33 -0700

    bumped rspec version, ignore my sudo draft method, parameterized sudo_path in spec

--- spec/backend/ssh/build_command_spec.rb
+++ spec/backend/ssh/build_command_spec.rb
@@ -30,29 +30,32 @@ describe 'build command with sudo' do
   end
 end
 
+# Alternate path for sudo command:
+sudo_path = '/usr/local/bin'
+
 describe 'build command with sudo on alternate path' do
   before :each do
     RSpec.configure do |c|
       ssh.stub(:options) { { :user => 'foo' } }
       c.ssh = ssh
-      c.sudo_path = '/usr/local/bin'
+      c.sudo_path = "#{sudo_path}"
     end
   end
 
 
   context 'command pattern 1a' do
     subject { backend.build_command('test -f /etc/passwd') }
-    it { should eq '/usr/local/bin/sudo test -f /etc/passwd' }
+    it { should eq "#{sudo_path}/sudo test -f /etc/passwd" }
   end
 
   context 'command pattern 2a' do
     subject { backend.build_command('test ! -f /etc/selinux/config || (getenforce | grep -i -- disabled && grep -i -- ^SELINUX=disabled$ /etc/selinux/config)') }
-    it { should eq '/usr/local/bin/sudo test ! -f /etc/selinux/config || (/usr/local/bin/sudo getenforce | grep -i -- disabled && /usr/local/bin/sudo grep -i -- ^SELINUX=disabled$ /etc/selinux/config)' }
+    it { should eq "#{sudo_path}/sudo test ! -f /etc/selinux/config || (#{sudo_path}/sudo getenforce | grep -i -- disabled && #{sudo_path}/sudo grep -i -- ^SELINUX=disabled$ /etc/selinux/config)" }
   end
 
   context 'command pattern 3a' do
     subject { backend.build_command("dpkg -s apache2 && ! dpkg -s apache2 | grep -E '^Status: .+ not-installed$'") }
-    it { should eq "/usr/local/bin/sudo dpkg -s apache2 && ! /usr/local/bin/sudo dpkg -s apache2 | grep -E '^Status: .+ not-installed$'" }
+    it { should eq "#{sudo_path}/sudo dpkg -s apache2 && ! #{sudo_path}/sudo dpkg -s apache2 | grep -E '^Status: .+ not-installed$'" }
   end
 
 end
