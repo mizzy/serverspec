d3ec54e45b20267ad957572c9b6a8eba84ca7311
worldostugs <stuart.tattershall@fil.com>
2013-09-10 17:43:07 +0100

    Aix support added

---
+++ lib/serverspec/commands/aix.rb
@@ -0,0 +1,36 @@
+require 'shellwords'
+
+module Serverspec
+  module Commands
+    class Aix < Base
+#
+# SJT Aix class
+#
+      class NotImplementedError < Exception; end
+
+      def check_access_by_user(file, user, access)
+        "su -s sh -c \"test -#{access} #{file}\" #{user}"
+      end
+
+      def check_enabled(service,level=nil)
+         "lssrc -s #{escape(service)} | grep active"
+      end
+       
+      def check_running(service)
+         "ps -ef | grep -v grep | grep #{escape(service)}"
+      end
+
+      def check_installed(package,version=nil)
+        "lslpp -L #{escape(package)}"
+      end
+
+      def check_listening(port)
+        regexp = "*.#{port} "
+        "netstat -an -f inet | awk '{print $4}' | grep  -- #{regexp}"
+        #"netstat -an -f inet | awk '{print $4}' | grep  -- #{escape(regexp)}"
+      end
+
+
+    end
+  end
+end
---
+++ spec/aix/command_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+include Serverspec::Helper::RedHat
+
+describe command('cat /etc/resolv.conf') do
+  let(:stdout) { "nameserver 127.0.0.1\r\n" }
+  it { should return_stdout("nameserver 127.0.0.1") }
+  its(:command) { should eq 'cat /etc/resolv.conf' }
+end
+
+describe 'complete matching of stdout' do
+  context command('cat /etc/resolv.conf') do
+    let(:stdout) { "foocontent-should-be-includedbar\r\n" }
+    it { should_not return_stdout('content-should-be-included') }
+  end
+end
+
+describe 'regexp matching of stdout' do
+  context command('cat /etc/resolv.conf') do
+    let(:stdout) { "nameserver 127.0.0.1\r\n" }
+    it { should return_stdout(/127\.0\.0\.1/) }
+  end
+end
+
+describe command('cat /etc/resolv.conf') do
+  let(:stdout) { "No such file or directory\r\n" }
+  it { should return_stderr("No such file or directory") }
+  its(:command) { should eq 'cat /etc/resolv.conf' }
+end
+
+describe 'complete matching of stderr' do
+  context command('cat /etc/resolv.conf') do
+    let(:stdout) { "No such file or directory\r\n" }
+    it { should_not return_stdout('file') }
+  end
+end
+
+describe 'regexp matching of stderr' do
+  context command('cat /etc/resolv.conf') do
+    let(:stdout) { "No such file or directory\r\n" }
+    it { should return_stderr(/file/) }
+  end
+end
+
+describe command('cat /etc/resolv.conf') do
+  it { should return_exit_status 0 }
+  its(:command) { should eq 'cat /etc/resolv.conf' }
+end
