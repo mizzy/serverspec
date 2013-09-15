6b5b8c8357f05105dfb055bac253bf556246bd64
pat <podenski@me.com>
2013-09-15 11:44:16 -0700

    added check_file_md5checksum for solaris 11

--- lib/serverspec/commands/solaris11.rb
+++ lib/serverspec/commands/solaris11.rb
@@ -2,6 +2,11 @@ module Serverspec
   module Commands
     class Solaris11 < Solaris
       # Please implement Solaris 11 specific commands
+
+      def check_file_md5checksum(file, expected)
+        "md5sum #{escape(file)} | grep -iw -- #{escape(expected)}"
+      end
+
     end
   end
 end
