67e59fecc0aa19200a900352021becbbdd3df7a3
Ryosuke Hamaji <aibou7251@gmail.com>
2013-09-17 15:46:17 +0900

    modified to be able to change the message of pass prompt.

--- lib/serverspec/configuration.rb
+++ lib/serverspec/configuration.rb
@@ -1,7 +1,7 @@
 module Serverspec
   module Configuration
     class << self
-      VALID_OPTIONS_KEYS = [:path, :pre_command, :stdout, :stderr, :sudo_path].freeze
+      VALID_OPTIONS_KEYS = [:path, :pre_command, :stdout, :stderr, :sudo_path, :passprompt].freeze
       attr_accessor(*VALID_OPTIONS_KEYS)
     
       def defaults
