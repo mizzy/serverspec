35f9a89151b25b53ad333606a806c3f02777005b
Gosuke Miyashita <gosukenator@gmail.com>
2013-09-23 13:28:36 +0900

    Stringify method name
    With Ruby 1.8, method name is simbol and doesn't match with any strings

--- lib/serverspec/commands/windows.rb
+++ lib/serverspec/commands/windows.rb
@@ -12,7 +12,7 @@ module Serverspec
       }
 
       def method_missing method, *args
-        raise NotSupportedError.new "#{method} currently not supported in Windows os" if method =~ /^check_.+/
+        raise NotSupportedError.new "#{method} currently not supported in Windows os" if method.to_s =~ /^check_.+/
         super(method, *args)
       end
 
