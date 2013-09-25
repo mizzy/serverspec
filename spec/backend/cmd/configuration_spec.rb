42a14a1d968d7bb569d7cce9d15fc2e2d5dd2768
Gosuke Miyashita <gosukenator@gmail.com>
2013-09-25 22:07:06 +0900

    Fix backend specs to use configuration based backend helper

--- spec/backend/cmd/configuration_spec.rb
+++ spec/backend/cmd/configuration_spec.rb
@@ -1,8 +1,10 @@
 require 'spec_helper'
 require 'support/powershell_command_runner'
 
-include Serverspec::Helper::Cmd
-include Serverspec::Helper::Windows
+RSpec.configure do |c|
+  c.os      = 'Windows'
+  c.backend = 'Cmd'
+end
 
 describe "Cmd" do
   it_behaves_like "a powershell command runner"
--- spec/backend/winrm/configuration_spec.rb
+++ spec/backend/winrm/configuration_spec.rb
@@ -1,8 +1,10 @@
 require 'spec_helper'
 require 'support/powershell_command_runner'
 
-include Serverspec::Helper::WinRM
-include Serverspec::Helper::Windows
+RSpec.configure do |c|
+  c.os      = 'Windows'
+  c.backend = 'WinRM'
+end
 
 describe "WinRM" do
   it_behaves_like "a powershell command runner"
