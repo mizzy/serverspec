3022dea571aacb93fe24eff5c48609aea09ea56e
Gosuke Miyashita <gosukenator@gmail.com>
2013-09-25 18:52:29 +0900

    Fix redhat specs to use configuration based backend helper

--- spec/redhat/linux_kernel_parameter_spec.rb
+++ spec/redhat/linux_kernel_parameter_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
 RSpec.configure do |c|
-  c.os = 'RedHat'
+  c.os      = 'RedHat'
+  c.backend = 'Exec'
 end
 
 describe linux_kernel_parameter('net.ipv4.tcp_syncookies') do
