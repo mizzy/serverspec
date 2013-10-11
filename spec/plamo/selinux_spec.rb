36f0ccd0bb5652d32ed960df62cb2c47fcc073d2
tenforward <karma@jazz.email.ne.jp>
2013-10-12 02:21:25 +0900

    Add support Plamo Linux

---
+++ spec/plamo/selinux_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+RSpec.configure do |c|
+  c.os = 'Plamo'
+end
+
+describe selinux do
+  it { should be_enforcing }
+  its(:command) { should eq "getenforce | grep -i -- enforcing && grep -i -- ^SELINUX=enforcing$ /etc/selinux/config" }
+end
+
+describe selinux do
+  it { should be_permissive }
+  its(:command) { should eq "getenforce | grep -i -- permissive && grep -i -- ^SELINUX=permissive$ /etc/selinux/config" }
+end
+
+describe selinux do
+  it { should be_disabled }
+  its(:command) { should eq "test ! -f /etc/selinux/config || (getenforce | grep -i -- disabled && grep -i -- ^SELINUX=disabled$ /etc/selinux/config)" }
+end
