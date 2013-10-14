f43e5a8e10477d599583fb4573893f67d7bfc671
Rick Hull <rick@cloudscaling.com>
2013-10-14 16:55:27 -0700

    add multiline match support
    split command output on \n and strip to take care of any CR issues

--- lib/serverspec/type/command.rb
+++ lib/serverspec/type/command.rb
@@ -1,6 +1,23 @@
 module Serverspec
   module Type
     class Command < Base
+      # args can be strings (full, exact match) or regex
+      # In order to return true, all args must match at least one line
+      def match_multiline?(*args)
+        lines = backend.run_command(@name).fetch(:stdout).split("\n").map { |l|
+          l.strip
+        }
+        args.each { |a|
+          found = false
+          lines.each { |l|
+            break if found
+            found = (a.is_a?(Regexp) ? l.match(a) : l == a)
+          }
+          return false unless found
+        }
+        true
+      end
+
       def return_stdout?(content)
         ret = backend.run_command(@name)
         if content.instance_of?(Regexp)
@@ -24,7 +41,7 @@ module Serverspec
 
       def return_exit_status?(status)
         ret = backend.run_command(@name)
-        ret[:exit_status].to_i == status
+        ret[:exit_status] == status.to_s
       end
     end
   end
