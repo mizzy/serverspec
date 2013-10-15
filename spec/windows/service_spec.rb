fc12cdc4f81ac7baf30ed2247062191f8cecefc7
Silvio Montanari <montanari.silvio@gmail.com>
2013-10-15 14:46:04 +1100

    more specs for service type

--- spec/windows/service_spec.rb
+++ spec/windows/service_spec.rb
@@ -22,3 +22,12 @@ end
 describe service('invalid-daemon') do
   it { should_not be_running }
 end
+
+describe service('Test service') do
+  it "should raise error if trying to check service process controller" do
+   expect { should be_running.under('supervisor') }.to raise_error
+ end
+  it "should raise error if trying to check service monitoring" do
+   expect { should_not be_monitored_by('monit') }.to raise_error
+ end
+end
