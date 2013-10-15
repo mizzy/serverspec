9052642a696a39d6cedb9dde5034f7d60c27e0ba
Rick Hull <rick@cloudscaling.com>
2013-10-14 17:13:25 -0700

    refactor matching into class methods
    drastically simplify instance method impls
    class methods lend themselves to testing
    acknowledge that return_stderr *is* just an alias

--- lib/serverspec/type/command.rb
+++ lib/serverspec/type/command.rb
@@ -1,47 +1,37 @@
 module Serverspec
   module Type
     class Command < Base
+      def self.match?(actual, expected)
+        expected.is_a?(Regexp) ? actual =~ expected : actual.strip == expected
+      end
+
       # args can be strings (full, exact match) or regex
       # In order to return true, all args must match at least one line
-      def match_multiline?(*args)
-        lines = backend.run_command(@name).fetch(:stdout).split("\n").map { |l|
-          l.strip
-        }
+      def self.match_multiline?(actual, *args)
+        lines = actual.split("\n").map { |l| l.strip }
         args.each { |a|
           found = false
-          lines.each { |l|
-            break if found
-            found = (a.is_a?(Regexp) ? l.match(a) : l == a)
-          }
+          lines.each { |l| break if found = match?(l, a) }
           return false unless found
         }
         true
       end
 
-      def return_stdout?(content)
-        ret = backend.run_command(@name)
-        if content.instance_of?(Regexp)
-          ret[:stdout] =~ content
-        else
-          ret[:stdout].strip == content
-        end
+      def match_multiline?(*args)
+        self.class.match_multiline?(backend.run_command(@name)[:stdout], *args)
       end
 
-      def return_stderr?(content)
-        ret = backend.run_command(@name)
-        # In ssh access with pty, stderr is merged to stdout
-        # See http://stackoverflow.com/questions/7937651/receiving-extended-data-with-ssh-using-twisted-conch-as-client
-        # So I use stdout instead of stderr
-        if content.instance_of?(Regexp)
-          ret[:stdout] =~ content
-        else
-          ret[:stdout].strip == content
-        end
+      def return_stdout?(content)
+        self.class.match?(backend.run_command(@name)[:stdout], content)
       end
+
+      # In ssh access with pty, stderr is merged to stdout
+      # See http://stackoverflow.com/questions/7937651/receiving-extended-data-with-ssh-using-twisted-conch-as-client
+      # So I use stdout instead of stderr
+      alias_method :return_stderr?, :return_stdout?
 
       def return_exit_status?(status)
-        ret = backend.run_command(@name)
-        ret[:exit_status] == status.to_s
+        backend.run_command(@name)[:exit_status] == status.to_s
       end
     end
   end
