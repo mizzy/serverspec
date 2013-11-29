e81c1f4a2e9ba9098516ebd47d610575663ec08b
Gosuke Miyashita <gosukenator@gmail.com>
2013-11-29 17:34:18 +0900

    Remove attributes

--- lib/serverspec/helper.rb
+++ lib/serverspec/helper.rb
@@ -3,10 +3,6 @@ require 'etc'
 # Command helpers
 require 'serverspec/helper/detect_os'
 
-# Attributes helper
-require 'serverspec/helper/attributes'
-include Serverspec::Helper::Attributes
-
 # Subject type helper
 require 'serverspec/helper/type'
 include Serverspec::Helper::Type
