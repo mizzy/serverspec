4890d66ca27898ba42a7cc18d4b3cb05349314e7
Gosuke Miyashita <gosukenator@gmail.com>
2014-01-16 23:43:05 +0900

    Fix spec for specinfra v0.4.1

--- spec/redhat/iptables_spec.rb
+++ spec/redhat/iptables_spec.rb
@@ -4,7 +4,7 @@ include SpecInfra::Helper::RedHat
 
 describe iptables do
   it { should have_rule '-P INPUT ACCEPT'  }
-  its(:command) { should eq "iptables -S | grep -- -P\\ INPUT\\ ACCEPT" }
+  its(:command) { should eq "iptables -S | grep -- -P\\ INPUT\\ ACCEPT || iptables-save | grep -- -P\\ INPUT\\ ACCEPT" }
 end
 
 describe iptables do
@@ -13,7 +13,7 @@ end
 
 describe iptables do
   it { should have_rule('-P INPUT ACCEPT').with_table('mangle').with_chain('INPUT') }
-  its(:command) { should eq "iptables -t mangle -S INPUT | grep -- -P\\ INPUT\\ ACCEPT" }
+  its(:command) { should eq "iptables -t mangle -S INPUT | grep -- -P\\ INPUT\\ ACCEPT || iptables-save -t mangle | grep -- -P\\ INPUT\\ ACCEPT" }
 end
 
 describe iptables do
