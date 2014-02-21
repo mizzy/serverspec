045c62a26d2619ce74e9571c3fad0b9d322fff30
Brian Fletcher <brian.jf.fletcher@gmail.com>
2014-02-21 22:41:41 +0000

    fixes specs in ruby 1.8.7

--- lib/serverspec/type/uri.rb
+++ lib/serverspec/type/uri.rb
@@ -3,7 +3,7 @@ module Serverspec
     class Uri < Base
       def http_get_matches_status_code?(status_code)
         response = backend.run_command("curl -i #{@name}").stdout
-        /^HTTP\/1.\d (?<status_code>\d+)/.match(response)[:status_code] == status_code.to_s
+        /^HTTP\/1.\d (\d+)/.match(response)[1] == status_code.to_s
       end
     end
   end
