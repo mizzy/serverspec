c496ecbfa237863da3ca00e78319336f2048966a
Brian Fletcher <brian.jf.fletcher@gmail.com>
2014-02-21 22:11:54 +0000

    adding a uri type and matcher for status code

---
+++ spec/plamo/uri_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+include SpecInfra::Helper::Plamo
+
+describe uri('http://www.google.ie') do
+  let(:stdout) { "HTTP/1.1 200 OK" }
+  it { should return_status_code_on_get(200) }
+  its(:command) { should eq "curl -i http://www.google.ie" }
+end
+
+describe uri('http://www.gsoogle.ie') do
+  let(:stdout) { "HTTP/1.1 404" }
+  it { should return_status_code_on_get(404) }
+  its(:command) { should eq "curl -i http://www.gsoogle.ie" }
+end
