fdb9906cd3c502cc014608569de35f240339334a
Gosuke Miyashita <gosukenator@gmail.com>
2014-05-06 20:01:34 +0900

    Remove specs depending on specinfra

--- spec/solaris11/ipfilter_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-include SpecInfra::Helper::Solaris11
-
-describe ipfilter do
-  it { should have_rule 'pass in quick on lo0 all' }
-  its(:command) { should eq "ipfstat -io 2> /dev/null | grep -- pass\\ in\\ quick\\ on\\ lo0\\ all" }
-end
