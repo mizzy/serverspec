f79f21c6504b1f3f8ca32d0436bdaf5802a7802e
Gosuke Miyashita <gosukenator@gmail.com>
2014-06-07 23:33:52 +0900

    Fix matching condition of vagrant vms

--- lib/serverspec/setup.rb
+++ lib/serverspec/setup.rb
@@ -209,7 +209,7 @@ end
         list_of_vms = []
         if vagrant_list != ''
           vagrant_list.each_line do |line|
-            if match = /([\w-]+[\s]+)(created|not created|poweroff|running|saved)[\s](\(virtualbox\)|\(vmware\))/.match(line)
+            if match = /([\w-]+[\s]+)(created|aborted|not created|poweroff|running|saved)[\s](\(virtualbox\)|\(vmware\))/.match(line)
               list_of_vms << match[1].strip!
             end
           end
