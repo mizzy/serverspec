b16dfd9c8e4c639067089aa9946b95603bb5f411
Gabor Nagy <mail@aigeruth.hu>
2014-07-05 02:13:56 +0200

    Add local address matching support to be_listening.

--- lib/serverspec/matchers/be_listening.rb
+++ lib/serverspec/matchers/be_listening.rb
@@ -1,9 +1,13 @@
 RSpec::Matchers.define :be_listening do
   match do |port|
-    port.listening?(@with)
+    port.listening? @with, @local_address
   end
 
   chain :with do |with|
-    @with = with 
+    @with = with
+  end
+
+  chain :on do |local_address|
+    @local_address = local_address
   end
 end
