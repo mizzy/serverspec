2f76b689677a14d2139ed567ce8fed19afb43944
Gosuke Miyashita <gosukenator@gmail.com>
2014-07-16 00:24:02 +0900

    Fix group type to pass the spec

--- lib/serverspec/type/group.rb
+++ lib/serverspec/type/group.rb
@@ -2,11 +2,11 @@ module Serverspec
   module Type
     class Group < Base
       def exists?
-        @runner.check_group(@name)
+        @runner.check_group_exists(@name)
       end
 
       def has_gid?(gid)
-        @runner.check_gid(@name, gid)
+        @runner.check_group_has_gid(@name, gid)
       end
     end
   end
