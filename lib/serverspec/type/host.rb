a43a74ff24a592d5bd177e32a0311c32db9d0103
Gosuke Miyashita <gosukenator@gmail.com>
2014-07-16 00:31:44 +0900

    Fix host type

--- lib/serverspec/type/host.rb
+++ lib/serverspec/type/host.rb
@@ -2,15 +2,15 @@ module Serverspec
   module Type
     class Host < Base
       def resolvable?(type)
-        @runner.check_resolvable(@name, type)
+        @runner.check_host_is_resolvable(@name, type)
       end
 
       def reachable?(port, proto, timeout)
-        @runner.check_reachable(@name, port, proto, timeout)
+        @runner.check_host_is_reachable(@name, port, proto, timeout)
       end
 
       def ipaddress
-        @runner.get_ipaddress_of_host(@name).stdout.strip
+        @runner.get_host_ipaddress(@name).stdout.strip
       end
     end
   end
