4209e93b88fc717bef40ca86ff5d7a490d035aae
Gosuke Miyashita <gosukenator@gmail.com>
2014-07-18 23:31:54 +0900

    Fix user

--- lib/serverspec/type/user.rb
+++ lib/serverspec/type/user.rb
@@ -2,28 +2,34 @@ module Serverspec
   module Type
     class User < Base
       def exists?
-        @runner.check_user(@name)
+        @runner.check_user_exists(@name)
       end
 
       def belongs_to_group?(group)
-        @runner.check_belonging_group(@name, group)
+        @runner.check_user_is_belonging_to_group(@name, group)
       end
 
       def has_uid?(uid)
-        @runner.check_uid(@name, uid)
+        @runner.check_user_has_uid(@name, uid)
       end
 
       def has_home_directory?(path)
-        @runner.check_home_directory(@name, path)
+        @runner.check_user_has_home_directory(@name, path)
       end
 
       def has_login_shell?(shell)
-        @runner.check_login_shell(@name, shell)
+        @runner.check_user_has_login_shell(@name, shell)
       end
 
       def has_authorized_key?(key)
-        @runner.check_authorized_key(@name, key)
+        @runner.check_user_has_authorized_key(@name, key)
       end
     end
   end
 end
+
+
+
+
+
+
