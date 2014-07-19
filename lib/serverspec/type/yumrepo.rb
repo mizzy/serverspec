435a9719a29a37c324c3e77343d48d6b46c85294
Gosuke Miyashita <gosukenator@gmail.com>
2014-07-19 17:45:37 +0900

    Fix yumrepo

--- lib/serverspec/type/yumrepo.rb
+++ lib/serverspec/type/yumrepo.rb
@@ -2,11 +2,11 @@ module Serverspec
   module Type
     class Yumrepo < Base
       def exists?
-        @runner.check_yumrepo(@name)
+        @runner.check_yumrepo_exists(@name)
       end
 
       def enabled?
-        @runner.check_yumrepo_enabled(@name)
+        @runner.check_yumrepo_is_enabled(@name)
       end
     end
   end
