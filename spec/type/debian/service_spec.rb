d61ce3e4cb6be60a61eda6ba6ee80dedd6c2b66a
Gosuke Miyashita <gosukenator@gmail.com>
2014-07-20 18:15:57 +0900

    Fix Debian

---
+++ spec/type/debian/service_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+set :os, :family => 'debian'
+
+describe commands.command_class('service').create do
+  it { should be_an_instance_of(Specinfra::Command::Debian::Base::Service) }
+end
+
+describe service('sshd') do
+  it { should be_enabled }
+end
+
+describe service('invalid-service') do
+  it { should_not be_enabled }
+end
+
+describe service('sshd') do
+  it { should be_enabled.with_level(4) }
+end
+
+describe service('invalid-service') do
+  it { should_not be_enabled.with_level(4) }
+end
