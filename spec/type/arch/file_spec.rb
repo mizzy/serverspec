d7a7cee5a53d69a7cb307e137d79a0daa401c397
Gosuke Miyashita <gosukenator@gmail.com>
2014-07-20 14:40:13 +0900

    Fix Arch Linux

---
+++ spec/type/arch/file_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+set :os, :family => 'arch'
+
+describe commands.command_class('file').create do
+  it { should be_an_instance_of(Specinfra::Command::Arch::Base::File) }
+end
+
+describe file('/tmp') do
+  it { should be_readable.by_user('mail') }
+end
+
+describe file('/tmp') do
+  it { should_not be_readable.by_user('invalid-user') }
+end
+
+describe file('/tmp') do
+  it { should be_writable.by_user('mail') }
+end
+
+describe file('/tmp') do
+  it { should_not be_writable.by_user('invalid-user') }
+end
+
+
+describe file('/tmp') do
+  it { should be_executable.by_user('mail') }
+end
+
+describe file('/tmp') do
+  it { should_not be_executable.by_user('invalid-user') }
+end
