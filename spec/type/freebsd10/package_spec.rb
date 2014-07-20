f85e70f3a145f2a998f9bac1ee586b21b81a1b2d
Gosuke Miyashita <gosukenator@gmail.com>
2014-07-20 21:32:15 +0900

    Fix FreeBSD 10

---
+++ spec/type/freebsd10/package_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+set :os, :family => 'freebsd', :release => 10
+
+describe commands.command_class('package').create do
+  it { should be_an_instance_of(Specinfra::Command::Freebsd::V10::Package) }
+end
+
+describe package('httpd') do
+  it { should be_installed }
+end
+
+describe package('invalid-package') do
+  it { should_not be_installed }
+end
+
+describe package('httpd') do
+  it { should be_installed.with_version('2.2.15-28.el6') }
+end
+
+describe package('httpd') do
+  it { should_not be_installed.with_version('invalid-version') }
+end
+
+describe package('httpd') do
+  let(:stdout) { "2.2.15\n" }
+  its(:version) { should eq '2.2.15' }
+  its(:version) { should > '2.2.14' }
+  its(:version) { should < '2.2.16' }
+  its(:version) { should > '2.2.9' }
+end
