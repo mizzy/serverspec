dd5d5427c552caf25de0e40d9f82c28b37c8e9d1
Gosuke Miyashita <gosukenator@gmail.com>
2014-07-23 22:30:27 +0900

    Fix how to override failure message

--- lib/serverspec.rb
+++ lib/serverspec.rb
@@ -12,23 +12,19 @@ require 'rspec/core/formatters/base_formatter'
 
 module RSpec::Core::Notifications
   class FailedExampleNotification < ExampleNotification
-    def message_lines
-      @lines ||=
+    def failure_lines
+      @failure_lines ||=
         begin
           lines = ["Failure/Error: #{read_failed_line.strip}"]
           lines << "#{exception_class_name}:" unless exception_class_name =~ /RSpec/
           exception.message.to_s.split("\n").each do |line|
           lines << "  #{line}" if exception.message
+        end
           lines << "  #{example.metadata[:command]}"
           lines << "  #{example.metadata[:stdout]}" if example.metadata[:stdout]
           lines << "  #{example.metadata[:stderr]}" if example.metadata[:stderr]
+          lines
         end
-        if shared_group
-          lines << "Shared Example Group: \"#{shared_group.metadata[:shared_group_name]}\"" +
-            " called from #{backtrace_formatter.backtrace_line(shared_group.location)}"
-        end
-        lines
-      end
     end
   end
 end
