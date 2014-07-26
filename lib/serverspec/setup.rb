71a8f42505cf3f87e67f44c31eacf7c480486375
Gosuke Miyashita <gosukenator@gmail.com>
2014-07-26 22:00:30 +0900

    Env key is Symbol insted of String

--- lib/serverspec/setup.rb
+++ lib/serverspec/setup.rb
@@ -293,7 +293,7 @@ set :ssh_options, options
 <%- end -%>
 
 # Set environment variables
-# set :env, 'LANG' => 'C', 'LC_MESSAGES' => 'C' 
+# set :env, :LANG => 'C', :LC_MESSAGES => 'C' 
 
 # Set PATH
 # set :path, '/sbin:/usr/local/sbin:$PATH'
