41c27c956a119ccf75007b3fbe17bbdd466621e5
Gosuke Miyashita <gosukenator@gmail.com>
2014-08-04 22:06:12 +0900

    Fix indent

--- lib/serverspec.rb
+++ lib/serverspec.rb
@@ -18,8 +18,8 @@ module RSpec::Core::Notifications
           lines = ["Failure/Error: #{read_failed_line.strip}"]
           lines << "#{exception_class_name}:" unless exception_class_name =~ /RSpec/
           exception.message.to_s.split("\n").each do |line|
-          lines << "  #{line}" if exception.message
-        end
+            lines << "  #{line}" if exception.message
+          end
           lines << "  #{example.metadata[:command]}"
           lines << "  #{example.metadata[:stdout]}" if example.metadata[:stdout]
           lines << "  #{example.metadata[:stderr]}" if example.metadata[:stderr]
