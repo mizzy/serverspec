330326d5c04362edb06c0100b984def16b436659
Gosuke Miyashita <gosukenator@gmail.com>
2014-08-06 10:45:51 +0900

    Import #456 to 2.0 branch

--- lib/serverspec/type/iis_app_pool.rb
+++ lib/serverspec/type/iis_app_pool.rb
@@ -9,6 +9,30 @@ module Serverspec
         @runner.check_iis_app_pool_has_dotnet_version(@name, dotnet)
       end
 
+      def has_32bit_enabled?()
+        @runner.check_iis_app_pool_has_32bit_enabled(@name)
+      end
+
+      def has_idle_timeout?(minutes)
+        @runner.check_iis_app_pool_has_idle_timeout(@name, minutes)
+      end
+
+      def has_identity_type?(identity_type)
+        @runner.check_iis_app_pool_has_identity_type(@name, identity_type)
+      end
+
+      def has_periodic_restart?(minutes)
+        @runner.check_iis_app_pool_has_periodic_restart(@name, minutes)
+      end
+
+      def has_user_profile_enabled?()
+        @runner.check_iis_app_pool_has_user_profile(@name)
+      end
+
+      def has_username?(username)
+        @runner.check_iis_app_pool_has_username(@name, username)
+      end
+
       def to_s
         %Q[IIS Application Pool "#{@name}"]
       end
--- spec/type/windows/iis_app_pool_spec.rb
+++ spec/type/windows/iis_app_pool_spec.rb
@@ -5,10 +5,12 @@ set :backend, :cmd
 set :os, :family => 'windows'
 
 describe iis_app_pool('Default App Pool') do
-  it{ should exist }
+  it { should exist }
+  it { should have_dotnet_version('2.0') }
+  it { should have_32bit_enabled }
+  it { should have_idle_timeout(5) }
+  it { should have_identity_type('foo') }
+  it { should have_periodic_restart(60) }
+  it { should have_user_profile_enabled }
+  it { should have_username('user') }
 end
-
-describe iis_app_pool('Default App Pool') do
-  it{ should have_dotnet_version('2.0') }
-end
-
