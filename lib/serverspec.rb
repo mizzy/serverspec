e418162a7c324ab91a5d2c4a8c9251070ed092c9
Gosuke Miyashita <gosukenator@gmail.com>
2014-08-08 10:36:04 +0900

    Display host name when failure

--- lib/serverspec.rb
+++ lib/serverspec.rb
@@ -15,7 +15,8 @@ module RSpec::Core::Notifications
     def failure_lines
       @failure_lines ||=
         begin
-          lines = ["Failure/Error: #{read_failed_line.strip}"]
+          lines = ["On host `#{Specinfra.configuration.host}`"]
+          lines << "Failure/Error: #{read_failed_line.strip}"
           lines << "#{exception_class_name}:" unless exception_class_name =~ /RSpec/
           exception.message.to_s.split("\n").each do |line|
             lines << "  #{line}" if exception.message
