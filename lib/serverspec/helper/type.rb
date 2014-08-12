9fe0529969171f91c8b19e03439a9eadf86bffed
Gosuke Miyashita <gosukenator@gmail.com>
2014-08-12 11:56:05 +0900

    Fix line length

--- lib/serverspec/helper/type.rb
+++ lib/serverspec/helper/type.rb
@@ -2,10 +2,12 @@ module Serverspec
   module Helper
     module Type
       types = %w(
-        base cgroup command cron default_gateway file group host iis_website iis_app_pool interface
-        ipfilter ipnat iptables kernel_module linux_kernel_parameter lxc mail_alias
-        package php_config port ppa process routing_table selinux service user yumrepo
-        windows_feature windows_hot_fix windows_registry_key windows_scheduled_task zfs
+        base cgroup command cron default_gateway file group host
+        iis_website iis_app_pool interface ipfilter ipnat iptables
+        kernel_module linux_kernel_parameter lxc mail_alias package
+        php_config port ppa process routing_table selinux service user
+        yumrepo windows_feature windows_hot_fix windows_registry_key
+        windows_scheduled_task zfs
       )
 
       types.each {|type| require "serverspec/type/#{type}" }
