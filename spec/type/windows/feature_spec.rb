d583fdefafde6b40515c081e9046462b4449fdd7
Gosuke Miyashita <gosukenator@gmail.com>
2014-08-16 21:31:23 +0900

    Add spec

--- spec/type/windows/feature_spec.rb
+++ spec/type/windows/feature_spec.rb
@@ -7,3 +7,11 @@ set :os, :family => 'windows'
 describe windows_feature('Minesweeper') do
   it{ should be_installed }
 end
+
+describe windows_feature('IIS-Webserver') do
+  it{ should be_installed.by(:dism) }
+end
+
+describe windows_feature('Web-Webserver') do
+  it{ should be_installed.by(:powershell) }
+end
