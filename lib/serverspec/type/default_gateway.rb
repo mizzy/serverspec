b31e73e09eca68aece937b008e5243638d811136
Jasper Lievisse Adriaanse <jasper@humppa.nl>
2014-09-18 22:45:50 +0200

    Move OS-specific code for default_gateway from Serverspec to Specinfra

--- lib/serverspec/type/default_gateway.rb
+++ lib/serverspec/type/default_gateway.rb
@@ -1,15 +1,11 @@
 module Serverspec::Type
   class DefaultGateway < Base
     def ipaddress
-      ret = @runner.get_routing_table_entry(:destination => 'default')
-      ret.stdout =~ /^(\S+)(?: via (\S+))? dev (\S+).+(?:\r)?\n(?:default via (\S+))?/
-      $2 ? $2 : $4
+      @runner.get_default_gateway(:gateway)
     end
 
     def interface
-      ret = @runner.get_routing_table_entry(:destination => 'default')
-      ret.stdout =~ /^(\S+)(?: via (\S+))? dev (\S+).+(?:\r)?\n(?:default via (\S+))?/
-      $3
+      @runner.get_default_gateway(:interface)
     end
 
     def to_s
