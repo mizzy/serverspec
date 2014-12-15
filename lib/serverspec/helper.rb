88c0cc92ece009dae2626a66aa1a570c5cf3953e
tyler-ball <tyleraball@gmail.com>
2014-12-15 14:04:56 -0800

    Excluding top-level Serverspec DSL inclusion

--- lib/serverspec/helper.rb
+++ lib/serverspec/helper.rb
@@ -1,6 +1,5 @@
 # Subject type helper
 require 'serverspec/helper/type'
-extend Serverspec::Helper::Type
 class RSpec::Core::ExampleGroup
   extend Serverspec::Helper::Type
   include Serverspec::Helper::Type
