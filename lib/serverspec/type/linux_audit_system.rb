4cb087932da2ed8ae12932a756e10ac43db76678
Andreas Schmidt <andreas.schmidt@cassini.de>
2015-08-06 17:54:42 +0200

    modified for outputs of different versions of auditctl, specs for both

--- lib/serverspec/type/linux_audit_system.rb
+++ lib/serverspec/type/linux_audit_system.rb
@@ -15,19 +15,31 @@ module Serverspec::Type
       (!pid.nil? && pid.size > 0 && pid != '0')
     end
 
-    private
-
     def rules
       if @rules_content.nil?
-        @rules_content = @runner.run_command('/sbin/auditctl -l').stdout
-#.split("\n").map(&:chomp)
+        @rules_content = @runner.run_command('/sbin/auditctl -l').stdout || ''
       end
-      @rules_content || []
+      @rules_content
     end
 
+    private
+
     def status_of(part)
-      cmd = "/sbin/auditctl -s | grep \"^#{part}\" | awk '{ print $2 }'"
-	@runner.run_command(cmd).stdout.chomp
+      cmd = "/sbin/auditctl -s"
+      status_str = @runner.run_command(cmd).stdout.chomp
+      status_map = parse_status(status_str)
+      status_map[part] || ''
     end
+
+    def parse_status(status_str)
+      map = nil
+      if status_str =~ /^AUDIT_STATUS/ then
+        map = status_str.split(' ')[1..-1].inject({}) { |res,elem| a = elem.split('='); res.store(a[0],a[1] || ''); res }
+      else
+        map = status_str.split("\n").inject({}) { |res,elem| a = elem.split(' '); res.store(a[0],a[1] || ''); res } 
+      end
+      map
+    end
+
   end
 end
