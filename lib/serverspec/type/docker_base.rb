196a23a126c886d68e95a6897c6ec1d529eb90e4
Olle Jonsson <olle.jonsson@ecraft.com>
2016-04-27 15:50:00 +0200

    Memoize without duplication

--- lib/serverspec/type/docker_base.rb
+++ lib/serverspec/type/docker_base.rb
@@ -16,14 +16,12 @@ module Serverspec::Type
     end
 
     def inspection
-      return @inspection if @inspection
-      @inspection = ::MultiJson.load(get_inspection.stdout)[0]
+      @inspection ||= ::MultiJson.load(get_inspection.stdout)[0]
     end
 
     private
     def get_inspection
-      return @get_inspection if @get_inspection
-      @get_inspection = @runner.run_command("docker inspect #{@name}")
+      @get_inspection ||= @runner.run_command("docker inspect #{@name}")
     end
   end
 end
