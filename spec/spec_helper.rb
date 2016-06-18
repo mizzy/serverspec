073d78357363aee466a9cd5c37a53cefedbf4ed6
Itamar Hassin <ihassin@mac.com>
2016-06-18 08:37:09 -0400

    Removed suppressing of stdout as it fails for ruby 1.87

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -27,18 +27,18 @@ module Specinfra
   end
 end
 
-RSpec.configure do |config|
-  original_stderr = $stderr
-  original_stdout = $stdout
-
-  config.before(:all) do
-    # Redirect stderr and stdout
-    $stderr = File.open(File::NULL, "w")
-    $stdout = File.open(File::NULL, "w")
-  end
-
-  config.after(:all) do
-    $stderr = original_stderr
-    $stdout = original_stdout
-  end
-end
+# RSpec.configure do |config|
+#   original_stderr = $stderr
+#   original_stdout = $stdout
+#
+#   config.before(:all) do
+#     # Redirect stderr and stdout
+#     $stderr = File.open(File::NULL, "w")
+#     $stdout = File.open(File::NULL, "w")
+#   end
+#
+#   config.after(:all) do
+#     $stderr = original_stderr
+#     $stdout = original_stdout
+#   end
+# end
