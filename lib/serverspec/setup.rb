228f9195baced71853abd3d1effd1b70938f34f5
gemmaro <gemmaro.dev@gmail.com>
2025-01-15 19:50:09 +0900

    Update RSpec one liner syntax.
    * lib/serverspec/setup.rb: Update RSpec one liner syntax.  Use is_expected
    instead of should.  See also the article of rspec-expectations gem. [1]
    
    [1] https://github.com/rspec/rspec-expectations/blob/main/Should.md

--- lib/serverspec/setup.rb
+++ lib/serverspec/setup.rb
@@ -100,30 +100,30 @@ EOF
 require 'spec_helper'
 
 describe package('httpd'), :if => os[:family] == 'redhat' do
-  it { should be_installed }
+  it { is_expected.to be_installed }
 end
 
 describe package('apache2'), :if => os[:family] == 'ubuntu' do
-  it { should be_installed }
+  it { is_expected.to be_installed }
 end
 
 describe service('httpd'), :if => os[:family] == 'redhat' do
-  it { should be_enabled }
-  it { should be_running }
+  it { is_expected.to be_enabled }
+  it { is_expected.to be_running }
 end
 
 describe service('apache2'), :if => os[:family] == 'ubuntu' do
-  it { should be_enabled }
-  it { should be_running }
+  it { is_expected.to be_enabled }
+  it { is_expected.to be_running }
 end
 
 describe service('org.apache.httpd'), :if => os[:family] == 'darwin' do
-  it { should be_enabled }
-  it { should be_running }
+  it { is_expected.to be_enabled }
+  it { is_expected.to be_running }
 end
 
 describe port(80) do
-  it { should be_listening }
+  it { is_expected.to be_listening }
 end
 EOF
 
